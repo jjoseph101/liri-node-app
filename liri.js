62aa09b5fe8fb2bad5492304b070f29b40cf51c1
JimmyJ <jimmyj.inbox@gmail.com>
2017-01-20 15:40:28 -0600

    finished basic hw

--- liri.js
+++ liri.js
@@ -3,16 +3,19 @@
 var Twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
+var fs = require("fs");
+var commandTxt = [];
 
 //initialize twitter keys file
 var keys = require("./keys.js");
 
-//initialize twiter keys
+//initialize twitter keys
 var consumerK = keys.twitterKeys.consumer_key;
 var consumerS = keys.twitterKeys.consumer_secret;
 var atK = keys.twitterKeys.access_token_key;
 var atS = keys.twitterKeys.access_token_secret;
 
+//extra step to initialize twitter keys
 var client = new Twitter({
   consumer_key: consumerK,
   consumer_secret: consumerS,
@@ -35,40 +38,70 @@ console.log(mediaInput);
 console.log ("==================");
 
 //command execution
-if (commandType==="my-tweets") { //display last twenty tweets;
-	client.get('favorites/list', function(error, tweets, response) {
-  		if(error) throw error;
-  		console.log(JSON.stringify(tweets, null, 2));
-	});
-
-} else if (commandType==="spotify-this-song") { //display song info;
-	spotify.search({ type: 'track', query: mediaInput }, function(err, data) {
-    	if ( err ) {
-        	console.log('Error occurred: ' + err);
-        	return;
-    	}; 
-    	console.log(JSON.stringify(data, null, 2));
-	});
-
-} else if (commandType==="movie-this") { //display movie info;
-	if (typeof mediaInput !== 'undefined') { //if movie title entered
-		request('http://www.omdbapi.com/?t='+mediaInput+'&y=&plot=full&r=json', function (error, response, body) {
-  			if (!error && response.statusCode == 200) {
-    			console.log(body) // Show the HTML for omdb response. 
-  			};
-		});
-	} else { //if movie title not entered return info for "Mr. Nobody"
-		request('http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=full&r=json', function (error, response, body) {
-  			if (!error && response.statusCode == 200) {
-    			console.log(body) // Show the HTML for omdb response. 
-  			};
-		});
+function doIt (commandType) {
+	if (commandType==="do-what-it-says") {
+		fs.readFile("random.txt", "utf8", function(error, data){
+	        console.log(data);
+	        string=data;
+	        commandTxt = string.split(",");
+	        commandType=commandTxt[0];
+	        mediaInput=commandTxt[1]; 
+	        commandRead(commandType, mediaInput);
+	     });  
 	};
+};
 
-} else if (commandType==="do-what-it-says") { //do command in random.txt
 
 
-} else {
-	console.log("You did not enter a correct preset command.  Please try again.");
-	return;
-}
+//display last twenty tweets
+function myTweets () {
+
+		client.get('favorites/list', function(error, tweets, response) {
+	  		if(error) throw error;
+	  		console.log(JSON.stringify(tweets, null, 2));
+		})};
+
+//display song info
+function mySpot (mediaInput) {
+
+		spotify.search({ type: 'track', query: mediaInput }, function(err, data) {
+	    	if ( err ) {
+	        	console.log('Error occurred: ' + err);
+	        	return;
+	    	}; 
+	    	console.log(JSON.stringify(data, null, 2));
+})};
+
+//display movie info
+function movieInfo (mediaInput) {
+		if (typeof mediaInput !== 'undefined') { //if movie title entered
+			request('http://www.omdbapi.com/?t='+mediaInput+'&y=&plot=full&r=json', function (error, response, body) {
+	  			if (!error && response.statusCode == 200) {
+	    			console.log(body);
+	  			};
+			});
+		} else { //if movie title not entered return info for "Mr. Nobody"
+			request('http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=full&r=json', function (error, response, body) {
+	  			if (!error && response.statusCode == 200) {
+	    			console.log(body);
+	  			};
+			});
+		}};
+
+//command line arguments
+function commandRead (commandType, mediaInput) {
+	if (commandType=="my-tweets") {
+		myTweets();
+	} else if (commandType=="spotify-this-song") {
+		mySpot (mediaInput);
+	} else if (commandType=="movie-this") {
+		movieInfo (mediaInput);
+	} else if (commandType=="do-what-it-says") {
+		doIt (commandType);
+	} else {
+		//invalid commands passed
+		console.log("You did not enter a correct preset command.  Please try again.");
+		return;
+}};
+
+commandRead(commandType, mediaInput);
