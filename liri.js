aea0bc58abde9f7ddec7a36c4641be67a530920a
JimmyJ <jimmyj.inbox@gmail.com>
2017-01-20 17:53:39 -0600

    clean up

--- liri.js
+++ liri.js
@@ -21,7 +21,7 @@ var commandType = process.argv[2];
 var mediaInput = process.argv[3];
 
 //function to read command execution from file
-function doIt (commandType) {
+function doIt () {
 	fs.readFile("random.txt", "utf8", function(error, data){
         console.log(data);
         string=data;
@@ -29,49 +29,49 @@ function doIt (commandType) {
         commandType=commandTxt[0];
         mediaInput=commandTxt[1]; 
         commandRead(commandType, mediaInput);
-    });  
+    }); 
 
 };
 
 //function to display last twenty tweets
 function myTweets () {
-		client.get('favorites/list', function(error, tweets, response) {
-	  		if(error) throw error;
-	  		console.log(JSON.stringify(tweets, null, 2));
-	  		fs.appendFile("log.txt", "\r\n-Ran command to display last twenty tweets");
-		});
-	};
+	client.get('favorites/list', function(error, tweets, response) {
+  		if(error) throw error;
+  		console.log(JSON.stringify(tweets, null, 2));
+  		fs.appendFile("log.txt", "\r\n-Ran command to display last twenty tweets");
+	});
+};
 
 //function to display song info
 function mySpot (mediaInput) {
-		spotify.search({ type: 'track', query: mediaInput }, function(err, data) {
-	    	if ( err ) {
-	        	console.log('Error occurred: ' + err);
-	        	return;
-	    	}; 
-	    	console.log(JSON.stringify(data, null, 2));
-	  		fs.appendFile("log.txt", "\r\n-Ran spotify for the following song: " + mediaInput);
-		});
-	};
+	spotify.search({ type: 'track', query: mediaInput }, function(err, data) {
+    	if ( err ) {
+        	console.log('Error occurred: ' + err);
+        	return;
+    	}; 
+    	console.log(JSON.stringify(data, null, 2));
+  		fs.appendFile("log.txt", "\r\n-Ran spotify for the following song: " + mediaInput);
+	});
+};
 
 //function to display movie info
 function movieInfo (mediaInput) {
-		if (typeof mediaInput !== 'undefined') { //if movie title entered
-			request('http://www.omdbapi.com/?t='+mediaInput+'&y=&plot=full&r=json', function (error, response, body) {
-	  			if (!error && response.statusCode == 200) {
-	    			console.log(body);
-	    			fs.appendFile("log.txt", "\r\n-Ran OMDB for the following movie: " + mediaInput);
-	  			};
-			});
-		} else { //if movie title not entered return info for "Mr. Nobody"
-			request('http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=full&r=json', function (error, response, body) {
-	  			if (!error && response.statusCode == 200) {
-	    			console.log(body);
-	    			fs.appendFile("log.txt", "\r\n-Ran OMDB for the following movie: Mr.Nobody");
-	  			};
-			});
-		};
+	if (typeof mediaInput !== 'undefined') { //if movie title entered
+		request('http://www.omdbapi.com/?t='+mediaInput+'&y=&plot=full&r=json', function (error, response, body) {
+  			if (!error && response.statusCode == 200) {
+    			console.log(body);
+    			fs.appendFile("log.txt", "\r\n-Ran OMDB for the following movie: " + mediaInput);
+  			};
+		});
+	} else { //if movie title not entered return info for "Mr. Nobody"
+		request('http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=full&r=json', function (error, response, body) {
+  			if (!error && response.statusCode == 200) {
+    			console.log(body);
+    			fs.appendFile("log.txt", "\r\n-Ran OMDB for the following movie: Mr.Nobody");
+  			};
+		});
 	};
+};
 
 //function to read command line arguments
 function commandRead (commandType, mediaInput) {
