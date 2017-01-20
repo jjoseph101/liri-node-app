aea8ddd126316556c694a7671e250aa2f069ee38
JimmyJ <jimmyj.inbox@gmail.com>
2017-01-20 14:57:02 -0600

    set up twitter and spotify

--- liri.js
+++ liri.js
@@ -1,5 +1,62 @@
-var twitter = require("twitter");
+
+//initialize external libraries
+var Twitter = require("twitter");
 var spotify = require("spotify");
 var request = require("request");
 
+//initialize twitter keys file
 var keys = require("./keys.js");
+
+//initialize twiter keys
+var consumerK = keys.twitterKeys.consumer_key;
+var consumerS = keys.twitterKeys.consumer_secret;
+var atK = keys.twitterKeys.access_token_key;
+var atS = keys.twitterKeys.access_token_secret;
+
+var client = new Twitter({
+  consumer_key: consumerK,
+  consumer_secret: consumerS,
+  access_token_key: atK,
+  access_token_secret: atS
+});
+
+//grab command-line argument
+var commandType = process.argv[2];
+var mediaInput = process.argv[3];
+
+//sanity checking
+console.log ("-----------------");
+console.log(consumerK);
+console.log(consumerS);
+console.log(atK);
+console.log(atS);
+console.log(commandType);
+console.log(mediaInput);
+console.log ("==================");
+
+//command execution
+if (commandType==="my-tweets") { //display last twenty tweets;
+	client.get('favorites/list', function(error, tweets, response) {
+  		if(error) throw error;
+  		console.log(JSON.stringify(tweets, null, 2));
+	});
+
+} else if (commandType==="spotify-this-song") { //display song info;
+	spotify.search({ type: 'track', query: mediaInput }, function(err, data) {
+    	if ( err ) {
+        	console.log('Error occurred: ' + err);
+        	return;
+    	}; 
+    	console.log(JSON.stringify(data, null, 2));
+	});
+
+} else if (commandType==="movie-this") { //display movie info;
+
+
+} else if (commandType==="do-what-it-says") { //do command in random.txt
+
+
+} else {
+	console.log("You did not enter a correct preset command.  Please try again.");
+	return;
+}
